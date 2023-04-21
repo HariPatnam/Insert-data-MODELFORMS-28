e786be809eca3ec0d9be10c0f5f9edf90b5c2e08
HariPatnam <patnamhari5432@gmail.com>
2023-04-21 14:15:19 +0530

    MODELFORM

--- app/forms.py
+++ app/forms.py
@@ -10,7 +10,13 @@ class TOPICFORM(forms.ModelForm):
 class WEBPAGEFORM(forms.ModelForm):
     class Meta:
         model=WEBPAGE
-        fields='__all__'
+        #fields='__all__'
+        #fields=['topic_name','name']
+        exclude=['url']
+        #widgets={'email':forms.PasswordInput,'url':forms.Textarea}
+        labels={'topic_name':'TOPIC_NAME','name':'NAME','email':'EMAIL'}
+        help_texts={'topic_name': '... WRITE UR GOVT NAME','name':'...WE CAN"T WRITE YOUR INSTA IDS','email':'..PSYCHO_SAI@GAMIL.COM  DON"T WRITE THESE TYPE OF IDS'}
+        
 
 class ACCESSFORM(forms.ModelForm):
     class Meta:
